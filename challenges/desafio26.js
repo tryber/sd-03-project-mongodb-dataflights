5b1d9fdbfa3d112f2622a80aeabcb369696fb0e4
Hfreitas <hfreitas86@gmail.com>
2020-07-31 15:58:01 -0300

    retirando operação count

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,1 @@
-db.voos.deleteMany( {"empresa.nome": "GOL"}, {"passageiros.pagos": { $gte: 5}}, {"passageiros.pagos": { $lte: 10}}).count();
+db.voos.deleteMany( {"empresa.nome": "GOL"}, {"passageiros.pagos": { $gte: 5}}, {"passageiros.pagos": { $lte: 10}});

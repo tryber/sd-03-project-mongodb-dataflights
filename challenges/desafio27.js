c019c89949ea1f3b2b0e7360ca82eff1d44eaa82
DiegoRafaelAF <draf0314@gmail.com>
2020-07-31 17:59:16 -0300

    codeclimate

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,3 @@
 db.resumoVoos.insertOne({"empresa": "PASSAREDO", "totalVoosDomesticos": 4187});
+
 db.resumoVoos.findOne({"empresa": "PASSAREDO"}, {"_id": 0, "empresa": 1, "totalVoosDomesticos": 1});

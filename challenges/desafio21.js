f1bd62eeb62ebd75e91627a35abbe25ad279d557
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-31 20:37:26 -0300

    cc

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,1 +1,1 @@
-db.voos.find({ "litrosCombustivel": { $gte: 1000 }}, { "_id": false, "vooId": true }).limit(1);
+db.voos.find({ "litrosCombustivel": { $gte: 1000 }}, { "_id": 0, "vooId": true }).limit(1);

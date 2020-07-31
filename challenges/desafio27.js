938d2811723bfa9945aaead9677071666de0bc07
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-31 19:50:43 -0300

    cc

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,6 +1,9 @@
 db.resumoVoos.insertOne({
   "totalVoosDomesticos": db.voos.find(
-    { "natureza": "Doméstica", "empresa.nome": "PASSAREDO" }
+    {
+      "natureza": "Doméstica",
+      "empresa.nome": "PASSAREDO"
+    }
   ).count(),
   "empresa": "PASSAREDO"
 });

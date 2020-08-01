f83bf81408690d9b8bb0d5bbfc6692ed621ec254
potew <potew0@gmail.com>
2020-07-31 21:43:50 -0300

    Update desafio28.js

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -4,7 +4,10 @@
 
 // 1. Conte quantos voos da empresa `LATAM AIRLINES BRASIL` cujo campo `natureza` possua valor igual a `Doméstica` e crie uma query que insira na coleção `resumoVoos` um documento com os campos: `empresa` (nome da empresa) e `totalVoosDomesticos` (o total retornado anteriormente).
 
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: db.voos.count({ $and: [{ "empresa.nome": "LATAM AIRLINES BRASIL" }, { natureza: "Doméstica" }] }) });
+db.resumoVoos.insertOne({
+  "empresa": "LATAM AIRLINES BRASIL",
+  "totalVoosDomesticos": db.voos.count({ $and: [{ "empresa.nome": "LATAM AIRLINES BRASIL" }, { natureza: "Doméstica" }] })
+});
 
 // 2. Em uma segunda query, retorne a `empresa` e o `totalVoosDomesticos` do primeiro documento presente na coleção `resumoVoos` em que a empresa seja `LATAM AIRLINES BRASIL`.
 

87e2f255994b6878c93eb03bedca306f9a506c68
Gabriel <dreadpool38@gmail.com>
2020-08-01 10:52:08 -0300

    more fixes

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insertOne({empresa:"PASSAREDO",totalvoosDomesticos:4187})
+db.resumoVoos.insertOne({empresa:"PASSAREDO",totalVoosDomesticos:4187})
 db.resumoVoos.find({empresa:"PASSAREDO"},{_id:0})

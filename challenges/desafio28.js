ed162f8afcc06750d84821577cc082ad05d8d79b
mirellaalves <mirella.alves@gmail.com>
2020-08-01 19:43:39 -0300

    code climate issues

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,9 +1,9 @@
+const total = db.voos.count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" });
+
 db.resumoVoos.insert(
   {
     "empresa": "LATAM AIRLINES BRASIL",
-    "totalVoosDomesticos": db.voos.count(
-      { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" }
-    )
+    "totalVoosDomesticos": total
   }
 );
 

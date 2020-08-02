81117b9be7c6d10b50d9a46205bb14301c1bfb1e
nato-x <alvares.renato@hotmail.com>
2020-08-02 18:03:03 -0300

    Add empresa.nome; Gol = mongoDeuBom!

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,7 +1,7 @@
-db.voos
-  .deleteMany({
-    $and: [
-      { "passageiros.pagos": { $gte: 5 } },
-      { "passageiros.pagos": { $lte: 10 } },
-    ],
-  });
+db.voos.deleteMany({
+  $and: [
+    { "empresa.nome": "GOL" },
+    { "passageiros.pagos": { $gte: 5 } },
+    { "passageiros.pagos": { $lte: 10 } },
+  ],
+});

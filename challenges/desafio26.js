2fd18810d2ab7e9c91b2d93a9800e6b555c6873d
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-03 11:18:11 -0300

    projeto data flights

---
+++ challenges/desafio26.js
@@ -0,0 +1,8 @@
+ines (7 sloc) 156 Bytes
+db.voos.deleteMany({
+  $and: [
+    { "empresa.nome": "GOL" },
+    { "passageiros.pagos": { $gte: 5 } },
+    { "passageiros.pagos": { $lte: 10 } },
+  ],
+});

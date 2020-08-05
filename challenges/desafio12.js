0897bc3649b5ebb94435160a6bf47bbc98d67467
Clayton Pereira <cpereiramt@gmail.com>
2020-08-05 17:58:57 -0400

    Salvando alteraçoes nos desafios do 10 ao 28

---
+++ challenges/desafio12.js
@@ -0,0 +1,5 @@
+db.voos.find( 
+    { $or: [     
+        { "aeroportoDestino.pais": "BRASIL"},      
+        { "aeroportoDestino.pais": "ARGENTINA"},
+        { "aeroportoDestino.pais": "CHILE"} ] }).count();
---
+++ challenges/desafio22.js
@@ -0,0 +1,11 @@
+db.voos.find(
+   { $and:[ 
+    { $or: [     
+        { "empresa.nome": {$eq: "DELTA AIRLINES"}},
+        { "empresa.nome": {$eq: "AMERICAN AIRLINES"}},
+    ] },
+    {"aeroportoOrigem.sigla":{$eq:"SBGR"}},
+    {"aeroportoDestino.sigla":{$eq:"KJFK"}}
+    ]
+   },{vooId:1, _id:0}
+   ).limit(1);

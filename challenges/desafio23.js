c509dc55fb43c57798cd96ee7d58e94349caae9a
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-08-06 10:51:01 -0300

    Correcting desafio23, desafio25 and desafio26

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,1 @@
-db.voos.findOne({"litrosCombustivel": {$exists: true, $gt: 1000}}, {"vooId": 1, "litrosCombustivel": 1, _id: 0});
+db.voos.findOne({"litrosCombustivel": {$exists: true, $not: {$gt: 1000}}}, {"vooId": 1, "litrosCombustivel": 1, _id: 0});
--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,7 @@
-db.voos.deleteMany({"empresa.nome": "GOL", "passageiros pagos": {$gte: 5, $lte: 10}}).count();
+  db.voos.deleteMany({
+  $and: [
+    { "empresa.nome": "GOL" },
+    { "passageiros.pagos": { $gte: 5 } },
+    { "passageiros.pagos": { $lte: 10 } },
+  ],
+});

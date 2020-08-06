0845ef11ae7017189ed54e9e7ad61c4403a6c0e6
luldelrey <rpgstrikered@gmail.com>
2020-08-06 12:23:27 -0300

    almost there

---
+++ challenges/desafio23.js
@@ -0,0 +1,13 @@
+db.voos.findOne(
+  {
+    $and : [
+      { "litrosCombustivel" : { $exists : true} },
+      { "litrosCombustivel" : { $not : { $gt: 1000 } } }
+    ]
+  },
+  {
+    "_id" : 0,
+    "vooId" : 1,
+    "litrosCombustivel" : 1,
+  }
+);
---
+++ challenges/desafio25.js
@@ -0,0 +1,7 @@
+db.inventory.deleteMany({
+  $and : [
+    { "empresa.nome" : "AZUL" },
+    { "litrosCombustivel" : { $exists : true} },
+    { "litrosCombustivel" : { $lt: 400 } }
+  ]},
+);

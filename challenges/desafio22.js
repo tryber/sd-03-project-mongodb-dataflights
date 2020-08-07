c2d54c4fc474ab18911b186754a6d6a8f1138f4a
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-07 03:40:13 -0300

    ajuste22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,7 +1,11 @@
 db.voos.findOne(
-    {$and:[{$or:[{"empresa.nome":"DELTA AIRLINES"},
-            {"empresa.nome":"AMERICAN AIRLINES"}]
-        },{$and:[{"aeroportoOrigem.sigla":"SBGR"},
-            {"aeroportoDestino.sigla":"KJFK"}]}]},
+    {$and:[{"empresa.nome" : {
+            $in: [
+          "DELTA AIRLINES",
+          "AMERICAN AIRLINES",
+            ]}},
+        {"aeroportoOrigem.sigla":"SBGR"},
+        {"aeroportoDestino.sigla":"KJFK"}
+    ]},
     {"vooId":1, "_id":0}
 );

0886228d2654986306ab6e9f3f62b119fce14a8d
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-08-10 22:49:12 -0300

    desafio23

---
+++ challenges/desafio23.js
@@ -0,0 +1,12 @@
+db.voos.findOne({
+  "litrosCombustivel": {
+    $not: { $gt: 1000 }
+  },
+  "litrosCombustivel": {
+    $exists: true
+  }
+},{
+  "_id": 0,
+  "vooId": 1,
+  "litrosCombustivel": 1
+});

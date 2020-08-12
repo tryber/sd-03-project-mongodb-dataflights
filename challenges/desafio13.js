6caeee7d6123ec42cdf1d4d6abe67dd5b0911879
tamagoshii <estelaap_martins@hotmail.com>
2020-08-12 15:42:52 -0300

    Code Climate voce nos odeia?!

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,7 +1,7 @@
-db.voos.find({
+db.voos.count({
   $nor: [
     { "aeroportoDestino.continente": "EUROPA" },
     { "aeroportoDestino.continente": "ÁSIA" },
     { "aeroportoDestino.continente": "OCEANIA" }
   ]
-}).count();
+});

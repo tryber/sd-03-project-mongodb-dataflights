5d1a10932f88c212920db77f5bf687672585678f
Luma Arruda <luma.oarruda@gmail.com>
2020-08-24 23:40:42 -0300

    desafios 21 - 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,15 @@
+// db.voos.count({
+//   "empresa.nome": "LATAM AIRLINES BRASIL",
+//   natureza: "Doméstica"
+// });
+
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: 20026
+});
+
+db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" } ,{
+  _id: 0,
+  empresa: 1,
+  totalVoosDomesticos: 1
+});
